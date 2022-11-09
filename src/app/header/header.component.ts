1d20b108b3923ce4651580f4c6cd63bfac044547
Joelma Regina <joelma_silvaa@hotmail.com>
2022-11-09 17:03:52 -0300

    Added routing to the pages of the app

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,20 +1,10 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
-  collapsed = true;
-  @Output() featureSelected = new EventEmitter<string>();
+export class HeaderComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-  onSelect(feature: string){
-    this.featureSelected.emit(feature);
-  }
 }
